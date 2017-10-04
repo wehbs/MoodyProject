74c19e466ff27ba8b62e52cbdc30fe883758da9b
wehbs <shamirwehbe@me.com>
2017-10-04 00:08:56 -0400

    almost done

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,5 @@
-$(function () {
+$(document).ready(function () {
+
 
 
     // jQuery for page scrolling feature - requires jQuery Easing plugin
@@ -47,7 +48,6 @@ $(function () {
                         scrollTop: $("#voice").offset().top
                     }, 1500, 'easeInOutExpo');
 
-                    
                     var faceResults = result.responses[0].faceAnnotations[0];
                     var anger = faceResults.angerLikelihood;
                     var joy = faceResults.joyLikelihood;
@@ -69,7 +69,7 @@ $(function () {
                         x = 'sad';
                     }
 
-                    googleVoice(x);
+                    setTimeout(googleVoice(x), 1000);
 
                 },
                 error: function (error) {
@@ -105,7 +105,10 @@ $(function () {
                 speechRecognizer.lang = 'en-US';
                 speechRecognizer.maxAlternatives = 1;
 
-                $("#button").on('click', function () {
+                $("#mic").on('click', function () {
+                    console.log($(this));
+
+                    $("#mic").css("animation", "mic-animate 2s linear infinite");
                     // event.preventDefault();
                     f();
                 });
@@ -115,39 +118,50 @@ $(function () {
                     speechRecognizer.onresult = function (event) {
                         for (var i = event.resultIndex; i < event.results.length; ++i) {
                             interimResults = event.results[i][0].transcript;
-                            mytext = $('#SpeechText').val();
+                            x = $('textarea').val();
                             console.log(event.results[i][0].transcript);
                             if (event.results[i].isFinal) {
                                 console.log(event.results[i].transcript);
                                 if (compare2string(event.results[i][0].transcript, "I m looking for some food")) {
                                     speechRecognizer.stop();
                                     speechSynthesis.speak(new SpeechSynthesisUtterance('go and  cook  some  food  for  your  self'));
-                                    $("#button").text('start');
                                 }
-                                if (compare2string(event.results[i][0].transcript, "go to google")) {
-                                    window.open("https://www.google.com/");
+                                if (compare2string(event.results[i][0].transcript, "go")) {
+                                    window.open("https://www.google.com/search?source=hp&q=" + x);
+                                    $('textarea').val("");
+                                    break;
+                                }
+                                if (compare2string(event.results[i][0].transcript, "stop")) {
+                                    speechRecognizer.stop();
+
+                                    $("#mic").css("animation", 'none');
+                                    break;
+                                }
+                                if (compare2string(event.results[i][0].transcript, "delete")) {
+                                    var lastIndex = x.lastIndexOf(" ");
+                                    x = x.substring(0, lastIndex);
+                                    $('textarea').val(x);
+                                    break;
+                                }
+                                if (compare2string(event.results[i][0].transcript, "delete all")) {
+                                    $('textarea').val("");
                                     break;
                                 }
                                 if (compare2string(event.results[i][0].transcript, "I want to watch a movie")) {
-                                    music();
+                                    foodMap('theatre');
+                                    break;
                                 }
                                 if (compare2string(event.results[i][0].transcript, "I want to eat something")) {
-                                    food();
+                                    foodMap('pizza');
+                                    break;
                                 }
-                                if (compare2string(event.results[i][0].transcript, "I want to go some where")) {
-                                    travel();
-                                }
-                                $('#SpeechText').val(mytext + " " + interimResults);
-                                $('#SpeechText').val(event.results[i][0].transcript);
+
+                                $('textarea').val(x + " " + interimResults);
+                                console.log("final results: " + event.results[i][0].transcript);
                             }
                         }
                     }
-                    $("#Stop").on('click', function () {
-                        speechRecognizer.stop();
-                        $('#SpeechText').val("");
-                    });
                 }
-
             }
 
             function compare2string(x, y) {
@@ -157,21 +171,98 @@ $(function () {
                     return false;
                 }
             }
-
-            function food() {
-                $("#A").append('<button id="google">food</button>');
-            }
-
-            function music() {
-                $("#A").append('<button id="google">music</button>');
-            }
-
-            function movie() {
-                $("#A").append('<button id="google">travel</button>');
-            }
         });
     }
 
+    function foodMap(search) {
+
+        // Scroll to voice section once results are produced for the image
+        $('html, body').stop().animate({
+            scrollTop: $("#mapSection").offset().top
+        }, 1500, 'easeInOutExpo');
+
+        $.getJSON("https://ipapi.co/json/",
+            function (json) {
+                // console.log(json);
+                var city = json.city;
+
+                $("iframe").attr("src", "https://www.google.com/maps/embed/v1/search?key=AIzaSyD0X2UTW5AczWoZ9-Wj517k9yvMZqBEeA4&q=" + search + "+in+" + city);
+            });
+    }
+
+    // var GW = new google.maps.LatLng(38.8824200, -77.116944);
+    // var map;
+    // var infowindow;
+    // var request;
+    // var service;
+    // var markers = [];
+
+    // function callback(results, status) {
+    //     if (status == google.maps.places.PlacesServiceStatus.OK) {
+    //         for (var i = 0; i < results.length; i++) {
+    //             var place = results[i];
+    //             createMarker(results[i]);
+    //         }
+    //     }
+    // }
+
+    // function createMarker(place) {
+    //     var placeLoc = place.geometry.location;
+    //     marker = new google.maps.Marker({
+    //         map: map,
+    //         position: place.geometry.location
+
+    //     });
+    //     google.maps.event.addListener(marker, 'click', function () {
+    //         var infowindow = new google.maps.InfoWindow({
+    //             content: place.name
+    //         });
+    //         infowindow.open(map, this);
+    //     });
+    //     return marker;
+    // }
+
+    // function clearResults(markers) {
+    //     for (var m in markers) {
+    //         markers[m].setmap(null)
+    //     }
+    //     markers = []
+    // }
+
+    // function foodMap(search) {
+
+    //     // Scroll to voice section once results are produced for the image
+    //     $('html, body').stop().animate({
+    //         scrollTop: $("#mapSection").offset().top
+    //     }, 1500, 'easeInOutExpo');
+
+    //     var request = {
+    //         location: GW,
+    //         radius: '500',
+    //         type: [search]
+    //     };
+
+    //     service = new google.maps.places.PlacesService(map);
+    //     service.nearbySearch(request, callback);
+
+    // }
+
+    // function initialize() {
+
+    //     map = new google.maps.Map(document.getElementById('map'), {
+    //         center: GW,
+    //         zoom: 16
+    //     });
+    //     request = {
+    //         location: GW,
+    //         radius: '500'
+    //     };
+
+    //     service = new google.maps.places.PlacesService(map);
+
+    // }
+
+    // google.maps.event.addDomListener(window, 'load', initialize);
 
 
 
